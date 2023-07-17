c25976ec4c068750a81c3bdff14cec473f164ea6
Mike Huang <huangjmike@gmail.com>
2023-07-17 08:12:30 -0700

    truncated sample files, added test converage

---
+++ tests/registration/test_register.py
@@ -0,0 +1,196 @@
+import tempfile
+import unittest
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+
+import ants
+import nibabel as nib
+import numpy as np
+
+from sptx_ccf_registration.registration.registration import (
+    Registration,
+    TransformType,
+)
+
+
+class TestRegistration(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        sample_path = Path(__file__).parent.parent / "sample_data"
+        cls.args = {
+            "output_name": "test",
+            "output_path": "tests/registration/test_output",
+            "merfish_files": {
+                "labels_broad_segmented": sample_path
+                / "mfish_labels_broad_segmented.nii.gz",
+                "labels_broad_unsegmented": sample_path
+                / "mfish_labels_broad_unsegmented.nii.gz",
+                "labels_landmark_segmented": sample_path
+                / "mfish_labels_landmark_segmented.nii.gz",
+                "labels_landmark_unsegmented": sample_path
+                / "mfish_labels_landmark_unsegmented.nii.gz",
+                "right_hemisphere": sample_path / "mfish_labels_right_hemisphere.nii.gz",
+            },
+            "ccf_files": {
+                "labels_broad": sample_path / "ccf_labels_broad.nii.gz",
+                "labels_landmark": sample_path / "ccf_labels_landmark.nii.gz",
+                "right_hemisphere": sample_path / "ccf_labels_right_hemisphere.nii.gz",
+            },
+            "labels_level": [0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+            "labels_replace_to": [1, -1, 1, 1, 1, 1, 1, 1, 1, 1],
+            "iteration_labels": [
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+                [72, 73],
+                [74],
+                [75, 76],
+                [113, 114],
+                [84],
+                [79],
+                [123, 54, 64, 121, 120, 78, 94],
+                [124],
+            ],
+        }
+
+    def setUp(self):
+        self.reg = Registration(**self.args)
+
+    def test_get_registration_params(self):
+        assert self.reg.get_registration_params(0) == ("SyN", (40, 20, 10), None)
+        assert self.reg.get_registration_params(1) == (
+            "SyNOnly",
+            (40, 20, 10),
+            "Identity",
+        )
+        assert self.reg.get_registration_params(2) == (
+            "SyNOnly",
+            (70, 40, 20),
+            "Identity",
+        )
+
+    def test_read_images(self):
+        merfish_images = self.reg.read_images(self.reg.merfish_files)
+        for key in self.reg.merfish_files.keys():
+            assert isinstance(merfish_images[key], ants.core.ANTsImage)
+
+    def test_is_empty(self):
+        empty_img = np.zeros((5, 5))
+        assert self.reg.is_empty(empty_img) is True
+        not_empty_img = np.ones((5, 5))
+        assert self.reg.is_empty(not_empty_img) is False
+
+    def test_select_images_and_labels(self):
+        merfish_images = {
+            k: nib.load(str(v)).get_fdata()
+            for k, v in self.args["merfish_files"].items()
+        }
+        ccf_images = {
+            k: nib.load(str(v)).get_fdata() for k, v in self.args["ccf_files"].items()
+        }
+
+        for iteration in range(10):  # assuming there are 10 iterations
+            selected_images = self.reg.select_images_and_labels(
+                iteration, merfish_images, ccf_images
+            )
+
+            # Test keys are preserved
+            self.assertEqual(set(selected_images.keys()), set(["mFish", "CCF"]))
+
+            # Test selected images have correct labels and labels are subsetted
+            # and merged correctly
+            for key, img in selected_images.items():
+                unique_labels = np.unique(img)
+                if self.reg.labels_replace_to[iteration] > 0 and iteration > 0:
+                    self.assertTrue(
+                        np.array_equal(
+                            unique_labels,
+                            np.array([0, self.reg.labels_replace_to[iteration]]),
+                        )
+                    )
+                elif iteration > 0:
+                    expected_labels = set(self.reg.iteration_labels[iteration] + [0])
+                    self.assertTrue(set(unique_labels).issubset(expected_labels))
+
+    def test_handle_empty_merfish_slice(self):
+        mock_merfish_slice = np.zeros((5, 5))
+        mock_ccf_slice = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
+        merfish_slice, ccf_slice = self.reg.handle_empty_merfish_slice(
+            mock_merfish_slice, mock_ccf_slice
+        )
+        assert (merfish_slice == mock_ccf_slice).all()
+        assert (ccf_slice == mock_ccf_slice).all()
+
+    def test_get_transform_path(self):
+        result = self.reg.get_transform_path(10, 1, "SYN")
+        assert "iter1" in result
+        assert "slice_transformations" in result
+        assert "test_SYN_slice10" in result
+
+    def test_create_transforms_slice(self):
+        for iteration in [0, 1]:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmpdir_path = Path(tmpdir)
+
+                def mocked_registration(*args, **kwargs):
+                    # Create the affine and warp transform dummy files
+                    fwd_affine_path = tmpdir_path / "fwd_affine_transform.mat"
+                    fwd_warp_path = tmpdir_path / "fwd_warp_transform.nii.gz"
+                    inv_affine_path = tmpdir_path / "inv_affine_transform.mat"
+                    inv_warp_path = tmpdir_path / "inv_warp_transform.nii.gz"
+                    with open(fwd_affine_path, "w") as f:
+                        f.write("dummy affine data")
+                    with open(fwd_warp_path, "w") as f:
+                        f.write("dummy warp data")
+                    with open(inv_affine_path, "w") as f:
+                        f.write("dummy affine data")
+                    with open(inv_warp_path, "w") as f:
+                        f.write("dummy warp data")
+
+                    # Return a dictionary with the paths to the dummy files
+                    return {
+                        "fwdtransforms": [str(fwd_warp_path), str(fwd_affine_path)],
+                        "invtransforms": [str(inv_warp_path), str(inv_affine_path)],
+                    }
+
+                with patch("ants.registration", side_effect=mocked_registration):
+                    merfish_slice = np.array(
+                        [[1, 2], [3, 4]], dtype=np.float32
+                    )  # dummy data
+                    ccf_slice = np.array(
+                        [[5, 6], [7, 8]], dtype=np.float32
+                    )  # dummy data
+                    z = 1  # dummy data
+
+                    # Create another temp directory for shutil.move
+                    with tempfile.TemporaryDirectory() as tmpdir2:
+                        self.reg.get_transform_path = MagicMock(
+                            return_value=Path(tmpdir2) / "transform.mat"
+                        )
+                        self.reg.create_transforms_slice(
+                            iteration, merfish_slice, ccf_slice, z
+                        )
+
+                        # Check if the files are correctly moved
+                        self.assertTrue((Path(tmpdir2) / "transform.mat").is_file())
+
+                        # Retrieve the arguments used for ants.registration call
+                        args, kwargs = ants.registration.call_args
+
+                        # assertions for the arguments
+                        self.assertTrue(
+                            np.array_equal(kwargs["fixed"], ants.from_numpy(ccf_slice))
+                        )
+                        self.assertTrue(
+                            np.array_equal(
+                                kwargs["moving"], ants.from_numpy(merfish_slice)
+                            )
+                        )
+
+                        if iteration == 0:
+                            self.assertEqual(
+                                kwargs["type_of_transform"], TransformType.SYN.value
+                            )  # replace 'SyN' with your value
+                        elif iteration > 0:
+                            self.assertEqual(
+                                kwargs["type_of_transform"], TransformType.SYNONLY.value
+                            )  # replace 'BSplineSyN' with your value
--- tests/segmentation/test_segment.py
+++ tests/segmentation/test_segment.py
@@ -1,55 +1,157 @@
+import json
 import unittest
+from pathlib import Path
+from unittest import mock
 
+import nibabel as nib
 import numpy as np
 
 from sptx_ccf_registration.segmentation.segment import SegmentSlice
 
 
 class TestSegmentSlice(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        sample_path = Path(__file__).parent.parent / "sample_data"
+        cls.args = {
+            "segmented_label_output_file": sample_path
+            / "mfish_labels_landmark_segmented.nii.gz",
+            "input_paths": {
+                "unsegmented_label_file": sample_path
+                / "mfish_labels_landmark_unsegmented.nii.gz",
+                "ccf_file": sample_path / "ccf_labels_landmark.nii.gz",
+                "itksnap_file_path": sample_path / "itksnap_ccf_landmark_file.txt",
+                "alpha_selection_path": sample_path
+                / "mfish_labels_landmark_alpha_selection_all.json",
+            },
+            "optimize_alpha": True,
+            "save_alpha_qc": True,
+        }
+
+        cls.unsegmented_labels_slice = (
+            nib.load(cls.args["input_paths"]["unsegmented_label_file"])
+            .get_fdata()[:, :, 0]
+            .astype(np.uint8)
+        )
+        cls.segmented_labels_slice = (
+            nib.load(cls.args["segmented_label_output_file"])
+            .get_fdata()[:, :, 0]
+            .astype(np.uint8)
+        )
+        cls.ccf_slice = nib.load(cls.args["input_paths"]["ccf_file"]).get_fdata()[
+            :, :, 0
+        ]
+        with open(cls.args["input_paths"]["alpha_selection_path"], "r") as fp:
+            cls.alpha_selection = json.load(fp)
+        cls.z = 0
 
     def setUp(self):
-        # Test data to be used across all tests
-        self.unsegmented_labels_slice = np.array([[0, 1], [2, 3]])
-        self.z = 2
+        self.segment_slice = SegmentSlice(
+            self.unsegmented_labels_slice,
+            self.z,
+            ccf_slice=self.ccf_slice,
+            optimize_alpha=True,
+            alpha_selection=self.alpha_selection,
+        )
 
     def test_initialization(self):
-        slice_obj = SegmentSlice(self.unsegmented_labels_slice, self.z)
-        self.assertEqual(slice_obj.z, self.z)
-        np.testing.assert_array_equal(slice_obj.unsegmented_labels_slice, self.unsegmented_labels_slice)
+        self.assertEqual(
+            self.segment_slice.y_dim, self.unsegmented_labels_slice.shape[0]
+        )
+        self.assertEqual(
+            self.segment_slice.x_dim, self.unsegmented_labels_slice.shape[1]
+        )
+        self.assertIsNotNone(self.segment_slice.points)
+        self.assertIsNotNone(self.segment_slice.labels)
+        self.assertIsNotNone(self.segment_slice.alpha_range)
+        self.assertEqual(self.segment_slice.z, self.z)
 
-    def test_invalid_input_shape(self):
-        with self.assertRaises(ValueError):
-            SegmentSlice(np.array([0, 1]), self.z)  # 1D array not 2D
-
-    def test_ccf_slice_mismatched_shape(self):
-        with self.assertRaises(ValueError):
-            SegmentSlice(self.unsegmented_labels_slice, self.z, ccf_slice=np.array([[0, 1, 2], [2, 3, 4]]))
-
-    def test_alpha_range(self):
-        slice_obj = SegmentSlice(self.unsegmented_labels_slice, self.z, min_alpha=0.1, max_alpha=0.5)
-        alpha_range = slice_obj.alpha_range
-        self.assertIsInstance(alpha_range, np.ndarray)
-        self.assertTrue(np.all(alpha_range >= 0.1))
-        self.assertTrue(np.all(alpha_range <= 0.5))
+    def test_unique_labels(self):
+        unique_labels = self.segment_slice.unique_labels
+        self.assertIsInstance(unique_labels, list)
+        self.assertTrue(all(isinstance(label, np.int64) for label in unique_labels))
 
     def test_find_optimal_alpha(self):
-        slice_obj = SegmentSlice(self.unsegmented_labels_slice, self.z, min_alpha=0.1, max_alpha=0.5, optimize_alpha=True, ccf_slice=self.unsegmented_labels_slice)
+        # pick one label to test
+        label = self.segment_slice.unique_labels[0]
+        alpha = self.segment_slice.find_optimal_alpha(label)
+        self.assertIsInstance(alpha, float)
+        self.assertTrue(
+            self.segment_slice.min_alpha <= alpha <= self.segment_slice.max_alpha
+        )
+        # test happy case
+        self.assertEqual(alpha, 0.2)
 
-        # Test the optimal alpha value for a specific label.
-        label = 1
-        alpha = slice_obj.find_optimal_alpha(label)
+    @mock.patch.object(SegmentSlice, "find_optimal_alpha", return_value=0.1)
+    def test_z_label_to_alpha(self, mock_find_optimal_alpha):
+        # happy case with alpha selection
+        z_label_to_alpha = self.segment_slice.z_label_to_alpha
+        self.assertIsInstance(z_label_to_alpha, dict)
+        self.assertTrue(
+            all(isinstance(alpha, (float, str)) for alpha in z_label_to_alpha.values())
+        )
+        self.assertEqual(z_label_to_alpha.get(str((self.z, 64))), 0.04)
+        # ignores optimal_alpha and picks from alpha_selection
+        mock_find_optimal_alpha.assert_not_called()
+        self.assertEqual(z_label_to_alpha.get(str((self.z, 64))), 0.04)
 
-        # Due to the complexity of find_optimal_alpha function, it might be hard to calculate an expected value directly.
-        # So we check if the output is within the expected range instead.
-        self.assertTrue(0.1 <= alpha <= 0.5)
+        # happy case without alpha selection
+        # test with optimize_alpha = True
+        segment_slice = SegmentSlice(
+            self.unsegmented_labels_slice,
+            self.z,
+            ccf_slice=self.ccf_slice,
+            optimize_alpha=True,
+            alpha_selection=None,
+        )
+        z_label_to_alpha = segment_slice.z_label_to_alpha
+        mock_find_optimal_alpha.assert_called()
+        mock_find_optimal_alpha.reset_mock()
+        self.assertEqual(z_label_to_alpha.get(str((self.z, 64))), 0.1)
 
-        # Test the case where there is no ccf_area (it's zero).
-        slice_obj.ccf_slice = np.zeros_like(self.unsegmented_labels_slice)
-        alpha = slice_obj.find_optimal_alpha(label)
+        # test with optimize_alpha = False
+        segment_slice = SegmentSlice(
+            self.unsegmented_labels_slice,
+            self.z,
+            ccf_slice=self.ccf_slice,
+            optimize_alpha=False,
+            alpha_selection=None,
+        )
+        z_label_to_alpha = segment_slice.z_label_to_alpha
+        mock_find_optimal_alpha.assert_not_called()
+        self.assertEqual(
+            z_label_to_alpha.get(str((self.z, 57))), self.segment_slice.default_alpha
+        )
 
-        # In this case, the function should return the default_alpha
-        self.assertEqual(alpha, slice_obj.default_alpha)
+    def test_max_density_labels(self):
+        max_density_labels = self.segment_slice.max_density_labels
+        self.assertIsInstance(max_density_labels, np.ndarray)
+        self.assertEqual(max_density_labels.shape, self.unsegmented_labels_slice.shape)
 
+    def test__label_points_to_binary_mask(self):
+        label_points = self.segment_slice.points[
+            self.segment_slice.labels == self.segment_slice.unique_labels[0]
+        ]
+        # test alpha that reuturns a mask
+        alpha = 0.2
+        binary_mask = self.segment_slice._label_points_to_binary_mask(
+            label_points, alpha, self.segment_slice.y_dim, self.segment_slice.x_dim
+        )
+        self.assertIsInstance(binary_mask, np.ndarray)
+        self.assertEqual(binary_mask.shape, self.unsegmented_labels_slice.shape)
 
-if __name__ == "__main__":
-    unittest.main()
+        # test alpha that returns None (alpha_shape.is_empty==True)
+        alpha = 1
+        binary_mask = self.segment_slice._label_points_to_binary_mask(
+            label_points, alpha, self.segment_slice.y_dim, self.segment_slice.x_dim
+        )
+        self.assertIsNone(binary_mask)
+
+    def test_segmented_labels_slice(self):
+        segmented_labels_slice = self.segment_slice.segmented_labels_slice
+        self.assertIsInstance(segmented_labels_slice, np.ndarray)
+        self.assertEqual(
+            segmented_labels_slice.shape, self.unsegmented_labels_slice.shape
+        )
+        # assert saved validated results
+        # np.testing.assert_array_equal(segmented_labels_slice, self.segmented_labels_slice)
